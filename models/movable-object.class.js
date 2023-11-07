a9b0fd7e17326107d85c4a707e4163e9a48cad7e
Stefan Jaroni <sjaroni@outlook.de>
2023-11-07 23:15:54 +0100

    add multiple pictures

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,7 +1,15 @@
 class MovableObject{
   x = 120;
-  y = 400;
+  y = 250;
+  width = 100;
+  height = 150;
   img;
+
+  // loadImage('img/test.png')
+  loadImage(path) {
+    this.img = new Image();
+    this.img.src = path;
+  }
 
   moveRight(){
     console.log('Moving right');
--- models/world.class.js
+++ models/world.class.js
@@ -1,12 +1,40 @@
 class World {
   character = new Character();
-  enemies = [
-    new Chicken(), 
-    new Chicken(), 
-    new Chicken(),
+  enemies = [new Chicken(), new Chicken(), new Chicken()];
+  clouds = [new Cloud(), new Cloud()];
+  backgroundObjects = [
+    new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0, 100),
   ];
+  canvas;
+  ctx;
+
+  constructor(canvas) {
+    this.ctx = canvas.getContext('2d');
+    this.canvas = canvas;
+    this.draw();
+  }
 
   draw() {
-    
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+    this.addToMap(this.character);
+    this.addObjectsToMap(this.clouds);
+    this.addObjectsToMap(this.enemies);
+    this.addObjectsToMap(this.backgroundObjects);
+
+    let self = this;
+    requestAnimationFrame(function () {
+      self.draw();
+    });
+  }
+
+  addObjectsToMap(objects) {
+    objects.forEach(o => {
+      this.addToMap(o);
+    });
+  }
+
+  addToMap(mo) {
+    this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
   }
 }
