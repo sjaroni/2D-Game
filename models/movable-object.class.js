cc59abcd3548fc52cce21e41245af9a9dce3ec7c
Stefan Jaroni <sjaroni@outlook.de>
2023-11-07 23:49:54 +0100

    add air

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,6 +1,6 @@
 class MovableObject{
   x = 120;
-  y = 250;
+  y = 280;
   width = 100;
   height = 150;
   img;
--- models/world.class.js
+++ models/world.class.js
@@ -3,7 +3,10 @@ class World {
   enemies = [new Chicken(), new Chicken(), new Chicken()];
   clouds = [new Cloud(), new Cloud()];
   backgroundObjects = [
+    new BackgroundObject('img/5_background/layers/air.png', 0),
     new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0),
+    new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 0),
+    new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0),
   ];
   canvas;
   ctx;
@@ -17,10 +20,10 @@ class World {
   draw() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
+    this.addObjectsToMap(this.backgroundObjects);
     this.addToMap(this.character);
     this.addObjectsToMap(this.clouds);
     this.addObjectsToMap(this.enemies);
-    this.addObjectsToMap(this.backgroundObjects);
 
     let self = this;
     requestAnimationFrame(function () {
