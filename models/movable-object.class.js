254bbc46595c953dd008f0e9ca00b131b30407f9
Stefan Jaroni <sjaroni@outlook.de>
2023-11-08 22:23:04 +0100

    animate-function

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -4,12 +4,23 @@ class MovableObject{
   width = 100;
   height = 150;
   img;
-
+  imageCache = {};
+  currentImage = 0;
+  
   // loadImage('img/test.png')
   loadImage(path) {
     this.img = new Image();
     this.img.src = path;
   }
+
+  loadImages(arr){    
+    arr.forEach(path => {      
+      let img = new Image();
+      img.src = path;
+      this.imageCache[path] = img;
+    });
+  }
+
 
   moveRight(){
     console.log('Moving right');
