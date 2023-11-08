1fe401ad0f84466998ee11a945b0d583d12aa2e2
Stefan Jaroni <sjaroni@outlook.de>
2023-11-08 22:32:27 +0100

    add different speed per chicken

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -2,7 +2,7 @@ class Cloud extends MovableObject{
 
   y = 20;
   height = 250;
-  width = 500;
+  width = 500;  
 
   constructor(){
     super().loadImage('img/5_background/layers/4_clouds/1.png');
@@ -12,9 +12,7 @@ class Cloud extends MovableObject{
   }
 
   animate(){
-    setInterval(() => {
-      this.x -= 0.15;
-    }, 1000 / 60);
+    this.moveLeft();
   }
 
 }
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -6,6 +6,7 @@ class MovableObject{
   img;
   imageCache = {};
   currentImage = 0;
+  speed = 0.15;
   
   // loadImage('img/test.png')
   loadImage(path) {
@@ -27,7 +28,9 @@ class MovableObject{
   }  
 
   moveLeft(){
-    
+    setInterval(() => {
+      this.x -= this.speed;
+    }, 1000 / 60); 
   }
 
 }
