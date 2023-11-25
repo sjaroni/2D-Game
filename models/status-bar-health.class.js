6a1187eefbe57b060424107b4f8976494b1b2691
Stefan Jaroni <sjaroni@outlook.de>
2023-11-25 14:57:39 +0100

    add counter on status-bar-coin

--- models/status-bar-health.class.js
+++ models/status-bar-health.class.js
@@ -6,12 +6,38 @@ class StatusBarHealth extends StatusIcons {
   textX = 150;
   width = 71;
   height = 69;
+  percentage = 100;
 
   IMAGES = [
     'img/7_statusbars/3_icons/icon_health.png'
   ];
   
   constructor(){    
-    super().loadImage(this.IMAGES[0]);    
-  }  
+    super().loadImage(this.IMAGES[0]);
+    this.setPercentage(100);
+  }
+
+  setPercentage(percentage){
+    this.percentage = percentage; // => 0 ... 5
+    this.text = percentage;
+    //let path = this.IMAGES[this.resolveImageIndex()];
+    //this.img = this.imageCache[path];
+  }
+  
+  resolveImageIndex(){
+    if(this.percentage == 100){
+      return 5;
+    } else if(this.percentage > 80){
+      return 4;
+    } else if(this.percentage > 60){
+      return 3;
+    } else if(this.percentage > 40){
+      return 2;
+    } else if(this.percentage > 20){
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+
 }
