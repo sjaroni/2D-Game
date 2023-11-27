3c1582ba73c0cafd4ca54ea922bde19c5cc13a35
Stefan Jaroni <sjaroni@outlook.de>
2023-11-27 16:38:47 +0100

    add throw-animation

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -1,6 +1,21 @@
 class ThrowableObject extends MovableObject {
+  offset = {
+    top: 15,
+    right: 40,
+    bottom: 25,
+    left: 20,
+  };
+
+  IMAGES_THROW = [
+    'img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png',
+    'img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png',
+    'img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png',
+    'img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png'
+  ];
+
   constructor(x, y, otherDirection) {
     super().loadImage('img/6_salsa_bottle/salsa_bottle.png');
+    this.loadImages(this.IMAGES_THROW);
     this.x = x;
     this.y = y;
     this.otherDirection = otherDirection;
@@ -11,10 +26,15 @@ class ThrowableObject extends MovableObject {
     this.throw();
   }
 
+  
+  
+  
   throw() {
-    this.speedY = 30; // 30
+    this.speedY = 30;
     this.applyGravity();
+    
     setInterval(() => {
+      this.playAnimation(this.IMAGES_THROW);
       if (this.otherDirection == false) {        
         this.x += 10;
       } else {                
