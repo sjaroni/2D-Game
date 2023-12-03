b21d461dd9019c162d21cbd2e96278d4d5b7b136
Stefan Jaroni <sjaroni@outlook.de>
2023-12-03 20:03:00 +0100

    adding endboss-status-bar

--- models/status-bar-endboss.class.js
+++ models/status-bar-endboss.class.js
@@ -1,18 +1,22 @@
 class StatusBarEndboss extends DrawableObject {
   
   IMAGES = [
-    'img/7_statusbars/2_statusbar_endboss/green.png',
-    'img/7_statusbars/2_statusbar_endboss/green.png',
-    'img/7_statusbars/2_statusbar_endboss/green.png',
-    'img/7_statusbars/2_statusbar_endboss/green.png',
-    'img/7_statusbars/2_statusbar_endboss/green.png',
-    'img/7_statusbars/2_statusbar_endboss/green.png'
+    'img/7_statusbars/1_statusbar/2_statusbar_health/green/0.png',
+    'img/7_statusbars/1_statusbar/2_statusbar_health/green/20.png',
+    'img/7_statusbars/1_statusbar/2_statusbar_health/green/40.png',
+    'img/7_statusbars/1_statusbar/2_statusbar_health/green/60.png',
+    'img/7_statusbars/1_statusbar/2_statusbar_health/green/80.png',
+    'img/7_statusbars/1_statusbar/2_statusbar_health/green/100.png'
   ];
-  
+
+  IMAGE = 'img/7_statusbars/3_icons/icon_health_endboss.png';
+
+  otherDirection = true;
   percentage = 100;
   constructor(){
     super();
     this.loadImages(this.IMAGES);
+    this.loadImage(this.IMAGE);
     this.x = 510;
     this.y = 20;
     this.width = 200;
