5f928c4a58c3f7e89ca9ad710c693f74621a283f
Stefan Jaroni <sjaroni@outlook.de>
2023-12-07 19:36:48 +0100

    add startscreen-container and images.js-file

---
+++ js/images.js
@@ -0,0 +1,8 @@
+
+// start_endscreen
+
+let START = 'img/9_intro_outro_screens/start/startscreen_2.png'
+let LOST = 'img/lost.png';
+let WIN = 'img/lost.png';
+
+// #start_endscreen background: url('START/WIN/LOST') no-repeat;
