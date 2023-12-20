a02adaf355b4b8f33fe09e62ee09e4a9e1aeef66
Stefan Jaroni <sjaroni@outlook.de>
2023-12-20 14:52:33 +0100

    add Eventlistener on panel

--- models/keyboard.class.js
+++ models/keyboard.class.js
@@ -13,11 +13,22 @@ class Keyboard {
       e.preventDefault();
       this.ARROWLEFT = true;
     });
-    
+
     document.getElementById('leftBtn').addEventListener('touchend', (e) =>{
       e.preventDefault();
       this.ARROWLEFT = false;
     });
+
+    document.getElementById('leftBtn').addEventListener('click', (e) =>{
+      e.preventDefault();
+      this.ARROWLEFT = true;
+    });
+
+    document.getElementById('leftBtn').addEventListener('mouseleave', (e) =>{
+      e.preventDefault();
+      this.ARROWLEFT = false;
+    });
+
 
     document.getElementById('rightBtn').addEventListener('touchstart', (e) =>{
       e.preventDefault();      
@@ -25,6 +36,16 @@ class Keyboard {
     });
     
     document.getElementById('rightBtn').addEventListener('touchend', (e) =>{
+      e.preventDefault();
+      this.ARROWRIGHT = false;
+    });
+
+    document.getElementById('rightBtn').addEventListener('click', (e) =>{
+      e.preventDefault();      
+      this.ARROWRIGHT = true;
+    });
+    
+    document.getElementById('rightBtn').addEventListener('mouseleave', (e) =>{
       e.preventDefault();
       this.ARROWRIGHT = false;
     });
@@ -40,6 +61,16 @@ class Keyboard {
       this.KEYD = false;
     });
 
+    document.getElementById('throwBtn').addEventListener('click', (e) =>{
+      e.preventDefault();      
+      this.KEYD = true;
+    });
+    
+    document.getElementById('throwBtn').addEventListener('mouseleave', (e) =>{
+      e.preventDefault();
+      this.KEYD = false;
+    });
+
     document.getElementById('jumpBtn').addEventListener('touchstart', (e) =>{
       e.preventDefault();      
       this.ARROWUP = true;
@@ -49,6 +80,17 @@ class Keyboard {
       e.preventDefault();
       this.ARROWUP = false;
     });
+
+    document.getElementById('jumpBtn').addEventListener('click', (e) =>{
+      e.preventDefault();      
+      this.ARROWUP = true;
+    });
+    
+    document.getElementById('jumpBtn').addEventListener('mouseleave', (e) =>{
+      e.preventDefault();
+      this.ARROWUP = false;
+    });
+
   }
 
 }
